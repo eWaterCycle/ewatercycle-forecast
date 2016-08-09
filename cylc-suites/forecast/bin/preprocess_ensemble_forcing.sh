808ac74f6dd1212ed7e96f422c8972b7ca32dce2
Niels Drost <n.drost@esciencecenter.nl>
2016-08-09 19:49:19 +0200

    some fixes to file locations

--- cylc-suites/forecast/bin/preprocess_ensemble_forcing.sh
+++ cylc-suites/forecast/bin/preprocess_ensemble_forcing.sh
@@ -28,25 +28,26 @@ do
     do
         cdo selparam,0.0.0 gep${ensembleMember}.t00z.pgrb2f${hour} tempEnsMem${ensembleMember}f{$hour}.grib2
     done
+
+    # All downloaded files only contain one time-step.
+    # Merge both precipitation and temperature files into one large file each for this ensemble member
+    cdo mergetime precipEnsMem${ensembleMember}f*.grib2 precipEnsMem${ensembleMember}.grib2
+    cdo mergetime tempEnsMem${ensembleMember}f*.grib2 tempEnsMem${ensembleMember}.grib2
+
 done
 
-# All downloaded files only contain one time-step.
-# Merge both precipitation and temperature files into one large file each.
-cdo mergetime precipEnsMem${ensembleMember}f*.grib2 precipEnsMem${ensembleMember}.grib2
-cdo mergetime tempEnsMem${ensembleMember}f*.grib2 tempEnsMem${ensembleMember}.grib2
-
 #calculate the ensmeble mean for both temperature and precipitation
-cdo ensmean forcingEnsemble/precipEnsMem??.grib2 precipEnsMeanOut.grib2
-cdo ensmean forcingEnsemble/tempEnsMem??.grib2 tempEnsMeanOut.grib2
+cdo ensmean precipEnsMem??.grib2 precipEnsMeanOut.grib2
+cdo ensmean tempEnsMem??.grib2 tempEnsMeanOut.grib2
 
 #for each ensemble member, calculate diff from mean, upscale to high res and add 
 #for details on operations done see the deterministic preprocessing script
 for ensembleMember in {01..20}
 do
-    cdo -f nc setrtoc,-100,0.0,0.0 -add forcingPrecipDailyOut.nc -remapnn,forcingPrecipDailyOut.nc -setmissval,1.0E20 -setname,precipitation -daysum -settime,00:00:00 -mulc,0.001 -sub forcingEnsemble/precipEnsMem${ensembleMember}.grib2 precipEnsMeanOut.grib2 forcingEnsemble/precipEnsMem${ensembleMember}.nc
+    cdo -f nc setrtoc,-100,0.0,0.0 -add forcingPrecipDailyOut.nc -remapnn,forcingPrecipDailyOut.nc -setmissval,1.0E20 -setname,precipitation -daysum -settime,00:00:00 -mulc,0.001 -sub precipEnsMem${ensembleMember}.grib2 precipEnsMeanOut.grib2 precipEnsMem${ensembleMember}.nc
 
-    cdo -f nc add forcingTempDailyOut.nc -remapnn,forcingTempDailyOut.nc -setmissval,1.0E20 -setname,temperature -settime,00:00:00 -setunit,C -dayavg -sub forcingEnsemble/tempEnsMem${ensembleMember}.grib2 tempEnsMeanOut.grib2 forcingEnsemble/tempEnsMem${ensembleMember}.nc
-
+    cdo -f nc add forcingTempDailyOut.nc -remapnn,forcingTempDailyOut.nc -setmissval,1.0E20 -setname,temperature -settime,00:00:00 -setunit,C -dayavg -sub tempEnsMem${ensembleMember}.grib2 tempEnsMeanOut.grib2 tempEnsMem${ensembleMember}.nc
+done
 
 # copy output to shared folder
 mkdir -p $IO_DIR/preprocess/ensemble
