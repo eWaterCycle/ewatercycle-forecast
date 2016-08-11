fd07092788d13c148eaf2f2ff0c28263fbd5e9e2
Niels Drost <n.drost@esciencecenter.nl>
2016-08-11 02:44:38 +0200

    finished forecast script, now also calls openda

--- cylc-suites/forecast/bin/run_forecast.sh
+++ cylc-suites/forecast/bin/run_forecast.sh
@@ -6,10 +6,6 @@
 #stop the script if we use an unset variable, or a command fails
 set -o nounset -o errexit
 
-#copy observations to current working directory
-OBSERVATION_FILE=$IO_DIR/preprocess/h14_$ISO_DATE.nc
-cp $OBSERVATION_FILE h14_observations.nc
-
 #copy forcing ensemble to current working directory
 ENSEMBLE_DIR=$IO_DIR/preprocess/ensemble
 cp $ENSEMBLE_DIR/* .
@@ -21,13 +17,25 @@ cp -r $INITIAL_DIR .
 
 #copy pcrglobwb config to current working directory
 
-PCRGLOBW_CONFIG=$IO_DIR/forecast/pcrglobwb.ini
+PCRGLOBWB_CONFIG=$IO_DIR/forecast/pcrglobwb_config.ini
 cp $PCRGLOBWB_CONFIG .
 
 #copy openda config to current working directory
 OPENDA_CONFIG_DIR=$IO_DIR/forecast/openda_config
 cp -r $OPENDA_CONFIG_DIR .
 
-#start openda
+#copy observations to openda observer folder
+OBSERVATION_FILE=$IO_DIR/preprocess/h14_$ISO_DATE.nc
+cp $OBSERVATION_FILE openda_config/stochObserver/h14_observations.nc
+
+WORKDIR=$PWD
+
+#start openda (assumes you have already setup openda previously)
+cd $OPENDADIR
+
+#we use a slightly changed version of oda_run that:
+# - has "set -o errexit" defined to make sure cylc gets passed any error status
+# - prints to the console instead of to a logfile
+oda_run_console.sh $WORKDIR/openda_config/enkf-seq-threaded.oda
 
 
