8647e9bf9902f841bf9cf00486782cb00ba1971f
Niels Drost <n.drost@esciencecenter.nl>
2016-08-12 02:24:10 +0200

    added 30min template, finalizing forecast run script

--- cylc-suites/forecast/bin/run_forecast.sh
+++ cylc-suites/forecast/bin/run_forecast.sh
@@ -4,42 +4,68 @@
 # Copies all needed files into the workdir of this job, then calls openda
 
 #stop the script if we use an unset variable, or a command fails
-set -o nounset -o errexit
 
-#copy forcing ensemble to current working directory
-ENSEMBLE_DIR=$IO_DIR/preprocess/ensemble
-cp $ENSEMBLE_DIR/* .
-
-#copy initial conditions to current working directory
-
-INITIAL_DIR=$IO_DIR/initial
-cp -r $INITIAL_DIR .
-
-#copy pcrglobwb config
-
-cp -r $PCRGLOBWB_MODEL_DIR PCR-GLOBWB
-
-mkdir model_template
-
-PCRGLOBWB_CONFIG=$IO_DIR/forecast/pcrglobwb_config.ini
-cp $PCRGLOBWB_CONFIG model_template
 
 #copy openda config to current working directory
 OPENDA_CONFIG_DIR=$IO_DIR/forecast/openda_config
 cp -r $OPENDA_CONFIG_DIR .
 
-#copy observations to openda observer folder
+#copy observations to openda config folder
 OBSERVATION_FILE=$IO_DIR/preprocess/h14_$ISO_DATE.nc
 cp $OBSERVATION_FILE openda_config/h14_observations.nc
 
+#copy model
+cp -r $PCRGLOBWB_MODEL_DIR PCR-GLOBWB
+
+#needed for openda
+mkdir model_template
+
+PCRGLOBWB_CONFIG=$IO_DIR/forecast/pcrglobwb_config.ini
+cp $PCRGLOBWB_CONFIG model_template
+
+for ensembleMember in {0..20}
+do
+    #also create a padded version of the number
+    printf -v ensembleMemberPadded '%02d' $ensembleMember
+
+    INSTANCE_DIR=work$ensembleMember
+    mkdir $INSTANCE_DIR
+
+    # copy config to instance
+    cp $PCRGLOBWB_CONFIG $INSTANCE_DIR
+
+    #copy initial state to instance
+    ENSEMBLE_INITIAL_DIR="$IO_DIR/initial/state-member$ensembleMemberPadded"
+    cp -r $ENSEMBLE_INITIAL_DIR $INSTANCE_DIR/initial
+
+    #copy forcings to instance
+    PRECIPITATION_FILE=$IO_DIR/preprocess/ensemble/precipEnsMem$ensembleMemberPadded.nc
+    TEMPERATURE_FILE=$IO_DIR/preprocess/ensemble/tempEnsMem$ensembleMemberPadded.nc
+
+    #Emsemble member 0 (the middle-of-the-road member) does not have its own forcings.
+    #Borrow forcings from member 1
+    if [[ "$ensembleMember" -eq "0" ]]
+    then
+        PRECIPITATION_FILE=$IO_DIR/preprocess/ensemble/precipEnsMem01.nc
+        TEMPERATURE_FILE=$IO_DIR/preprocess/ensemble/precipEnsMem01.nc
+    fi
+
+
+    cp $PRECIPITATION_FILE $INSTANCE_DIR/precipitation.nc
+    cp $TEMPERATURE_FILE $INSTANCE_DIR/temperature.nc
+done
+
+#remember workdir
 WORKDIR=$PWD
 
 #start openda (assumes you have already setup openda previously)
 cd $OPENDADIR
 
-#we use a slightly changed version of oda_run that:
-# - has "set -o errexit" defined to make sure cylc gets passed any error status
-# - prints to the console instead of to a logfile
+#We use a modified version of oda_run that:
+# - Has "set -o errexit" defined to make sure cylc gets passed any error status
+# - Prints to the console instead of to a logfile
+# - Does not set LD_LIBRARY_PATH (causing conflics as it contains a lot of basic libraries such as sqlite and netcdf)
+# - Sets java.library.path to still have native libraries available to OpenDA
 oda_run_console.sh $WORKDIR/openda_config/enkf-seq-threaded.oda
 
 
