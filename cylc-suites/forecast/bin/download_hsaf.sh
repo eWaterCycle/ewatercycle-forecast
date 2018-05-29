ce7f2270b769220961d93c1a36b0c6278d5efa73
Ronald van Haren <R.vanHaren@esciencecenter.nl>
2018-05-29 13:03:38 +0200

    use .netrc to specify ftp credentials for hsaf

--- cylc-suites/forecast/bin/download_hsaf.sh
+++ cylc-suites/forecast/bin/download_hsaf.sh
@@ -4,13 +4,12 @@
 #
 # uses environment variables:
 #     ISO_DATE, for date to download
-#     CREDENTIALS, for username:password for the hsaf site
 #     IO_DIR, for location of shared state files for this cycle
 
 #stop the script if we use an unset variable, or a command fails
 set -o nounset -o errexit
 
-#download bz2 file
-wget ftp://${CREDENTIALS}@ftphsaf.meteoam.it/h14/h14_cur_mon_grib/h14_${ISO_DATE}_0000.grib.bz2
+#download bz2 file, uses credentials as specified in ~/.netrc
+wget ftp://ftphsaf.meteoam.it/h14/h14_cur_mon_grib/h14_${ISO_DATE}_0000.grib.bz2
 
 cp h14_${ISO_DATE}_0000.grib.bz2 $IO_DIR/download/
